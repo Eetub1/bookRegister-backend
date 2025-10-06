ef6de16391a828a669f4a9fceac35070c773bd85
eeeeoino <eetu.oinonen04@gmail.com>
2025-10-06 22:37:31 +0300

    you can now create users and get all users

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 const config = require('./utils/config')
 const booksRouter = require('./controllers/books')
+const usersRouter = require('./controllers/users')
 
 const app = express()
 app.use(express.json())
@@ -9,5 +10,6 @@ app.use(express.json())
 mongoose.connect(config.MONGODB_URI)
 
 app.use('/api/books', booksRouter)
+app.use('/api/users', usersRouter)
 
 module.exports = app
--- controllers/books.js
+++ controllers/books.js
@@ -2,25 +2,26 @@ const booksRouter = require('express').Router()
 const Book = require('../models/book')
 
 booksRouter.get('/', async (request, response) => {
-    const books = await Book.find({})
-    response.json(books)
-    //response.send("Morojes")
+  const books = await Book.find({})
+  response.json(books)
+  //response.send("Morojes")
 })
 
 booksRouter.post('/', async (request, response) => {
-    const body = request.body
+  const body = request.body
 
-    console.log(body)
+  console.log(body)
 
-    const book = new Book({
-        title: body.title,
-        author: body.author,
-        year: body.year
-    })
+  const book = new Book({
+      title: body.title,
+      author: body.author,
+      year: body.year,
+      pages: body.pages
+  })
 
-    const savedBook = await book.save()
+  const savedBook = await book.save()
 
-    response.status(201).json(savedBook)
+  response.status(201).json(savedBook)
 })
 
 module.exports = booksRouter
