bbea0e8cb1c02896cc5c31ef89a9b86e6e4d8ad1
eeeeoino <eetu.oinonen04@gmail.com>
2025-10-06 23:08:22 +0300

    kirjautuminen onnistuu

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose')
 const config = require('./utils/config')
 const booksRouter = require('./controllers/books')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 
 const app = express()
 app.use(express.json())
@@ -11,5 +12,6 @@ mongoose.connect(config.MONGODB_URI)
 
 app.use('/api/books', booksRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 module.exports = app
--- utils/config.js
+++ utils/config.js
@@ -3,5 +3,6 @@ dotenv.config()
 
 const PORT = process.env.PORT
 const MONGODB_URI = process.env.MONGODB_URI
+const SECRET = process.env.SECRET
 
-module.exports = { PORT, MONGODB_URI }
+module.exports = { PORT, MONGODB_URI, SECRET }
